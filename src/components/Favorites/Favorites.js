e1e4203bedc89f0f7237c4472b999ce614fa21eb
vusal <MY_NAME@example.com>
2023-03-02 10:32:31 -0800

    Class componentler Function componentlere chevriliidi

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,27 +1,30 @@
-import React, { Component } from 'react';
-import './Favorites.css';
+import React from "react";
+import { useState } from "react";
+import "./Favorites.css";
 
+const Favorites = () => {
+  const [state, setState] = useState({
+    title: "Новый список",
+    movies: [{ imdbID: "tt0068646", title: "The Godfather", year: 1972 }],
+  });
 
-class Favorites extends Component {
-    state = {
-        title: 'Новый список',
-        movies: [
-            { imdbID: 'tt0068646', title: 'The Godfather', year: 1972 }
-        ]
-    }
-    render() { 
-        return (
-            <div className="favorites">
-                <input value="Новый список" className="favorites__name" />
-                <ul className="favorites__list">
-                    {this.state.movies.map((item) => {
-                        return <li key={item.id}>{item.title} ({item.year})</li>;
-                    })}
-                </ul>
-                <button type="button" className="favorites__save">Сохранить список</button>
-            </div>
-        );
-    }
-}
- 
+  return (
+    <div className="favorites">
+      <input value="Новый список" className="favorites__name" />
+      <ul className="favorites__list">
+        {state.movies.map((item) => {
+          return (
+            <li key={item.id}>
+              {item.title} ({item.year})
+            </li>
+          );
+        })}
+      </ul>
+      <button type="button" className="favorites__save">
+        Сохранить список
+      </button>
+    </div>
+  );
+};
+
 export default Favorites;
--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -1,43 +1,39 @@
-import React, { Component } from 'react';
-import './SearchBox.css';
+import React, { Component } from "react";
+import { useState } from "react";
+import "./SearchBox.css";
 
-class SearchBox extends Component {
-    state = {
-        searchLine: ''
-    }
-    searchLineChangeHandler = (e) => {
-        this.setState({ searchLine: e.target.value });
-    }
-    searchBoxSubmitHandler = (e) => {
-        e.preventDefault();
-    }
-    render() {
-        const { searchLine } = this.state;
+const SearchBox = () => {
+  const [searchLine, setSearchLine] = useState("");
+  const searchLineChangeHandler = (e) => {
+    this.setState({ searchLine: e.target.value });
+  };
+  const searchBoxSubmitHandler = (e) => {
+    e.preventDefault();
+  };
 
-        return (
-            <div className="search-box">
-                <form className="search-box__form" onSubmit={this.searchBoxSubmitHandler}>
-                    <label className="search-box__form-label">
-                        Искать фильм по названию:
-                        <input
-                            value={searchLine}
-                            type="text"
-                            className="search-box__form-input"
-                            placeholder="Например, Shawshank Redemption"
-                            onChange={this.searchLineChangeHandler}
-                        />
-                    </label>
-                    <button
-                        type="submit"
-                        className="search-box__form-submit"
-                        disabled={!searchLine}
-                    >
-                        Искать
-                    </button>
-                </form>
-            </div>
-        );
-    }
-}
- 
+  return (
+    <div className="search-box">
+      <form className="search-box__form" onSubmit={searchBoxSubmitHandler}>
+        <label className="search-box__form-label">
+          Искать фильм по названию:
+          <input
+            value={searchLine}
+            type="text"
+            className="search-box__form-input"
+            placeholder="Например, Shawshank Redemption"
+            onChange={searchLineChangeHandler}
+          />
+        </label>
+        <button
+          type="submit"
+          className="search-box__form-submit"
+          disabled={!searchLine}
+        >
+          Искать
+        </button>
+      </form>
+    </div>
+  );
+};
+
 export default SearchBox;
